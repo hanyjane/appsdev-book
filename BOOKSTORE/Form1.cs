9605fa1d908ba49ca111eca2ea2ece53941f7ef8
Abeey18 <abegailretuertomoniva@gmail.com>
2025-05-13 07:54:18 +0800

    1

--- BOOKSTORE/Form1.cs
+++ BOOKSTORE/Form1.cs
@@ -15,6 +15,14 @@ namespace BOOKSTORE
         public Form1()
         {
             InitializeComponent();
+
+            button1 = new Button();
+            button1.Name = "button1";
+            button1.Size = new Size(100, 40);
+            button1.Location = new Point(100, 100);
+            button1.MouseEnter += button1_MouseEnter;
+
+            Controls.Add(button1);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,6 +30,31 @@ namespace BOOKSTORE
              Login form = new Login(); //Hides
             form.Show();
             this.Hide();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Login newForm = new Login();
+            newForm.StartPosition = FormStartPosition.Manual;
+            newForm.Location = new Point(this.Right, this.Top);
+            newForm.Show();
+
+            Timer slideTimer = new Timer();
+            slideTimer.Interval = 10;
+
+            slideTimer.Tick += (s, ev) =>
+            {
+                if (newForm.Left > this.Left + 50)
+                {
+                    newForm.Left -= 20;
+                }
+                else
+                {
+                    slideTimer.Stop();
+                }
+            };
+
+            slideTimer.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
