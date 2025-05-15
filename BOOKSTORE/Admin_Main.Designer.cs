18c342181dd7927430bce530500abee777ffa5e9
Anna Cueva <annamae.cueva1016@outlook.com>
2025-05-16 06:27:35 +0800

    visual

--- BOOKSTORE/Admin_Main.Designer.cs
+++ BOOKSTORE/Admin_Main.Designer.cs
@@ -32,32 +32,26 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.btn_Update = new System.Windows.Forms.Button();
             this.btn_Delete = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.btn_Explore = new System.Windows.Forms.Button();
-            this.btn_Add = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.ViewCart = new System.Windows.Forms.Label();
-            this.LogOut = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LogOut = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Label();
+            this.Explore = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.flowLayoutPanel1.Controls.Add(this.btn_Update);
-            this.flowLayoutPanel1.Controls.Add(this.btn_Delete);
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(238, 244);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -66,9 +60,9 @@
             // 
             // btn_Update
             // 
-            this.btn_Update.Location = new System.Drawing.Point(3, 3);
+            this.btn_Update.Location = new System.Drawing.Point(556, 200);
             this.btn_Update.Name = "btn_Update";
-            this.btn_Update.Size = new System.Drawing.Size(95, 86);
+            this.btn_Update.Size = new System.Drawing.Size(95, 26);
             this.btn_Update.TabIndex = 1;
             this.btn_Update.Text = "Update";
             this.btn_Update.UseVisualStyleBackColor = true;
@@ -76,149 +70,31 @@
             // 
             // btn_Delete
             // 
-            this.btn_Delete.Location = new System.Drawing.Point(3, 95);
+            this.btn_Delete.Location = new System.Drawing.Point(657, 200);
             this.btn_Delete.Name = "btn_Delete";
-            this.btn_Delete.Size = new System.Drawing.Size(104, 38);
+            this.btn_Delete.Size = new System.Drawing.Size(104, 26);
             this.btn_Delete.TabIndex = 3;
             this.btn_Delete.Text = "Delete";
             this.btn_Delete.UseVisualStyleBackColor = true;
             this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.Cursor = System.Windows.Forms.Cursors.No;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Stencil", 11F);
-            this.button2.ForeColor = System.Drawing.Color.AliceBlue;
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.button2.Location = new System.Drawing.Point(511, 106);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(214, 53);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Logout";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // btn_Explore
-            // 
-            this.btn_Explore.BackColor = System.Drawing.Color.AliceBlue;
-            this.btn_Explore.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btn_Explore.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btn_Explore.Font = new System.Drawing.Font("Stencil", 11F);
-            this.btn_Explore.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.btn_Explore.Location = new System.Drawing.Point(533, 150);
-            this.btn_Explore.Name = "btn_Explore";
-            this.btn_Explore.Size = new System.Drawing.Size(211, 54);
-            this.btn_Explore.TabIndex = 0;
-            this.btn_Explore.Text = "Explore";
-            this.btn_Explore.UseVisualStyleBackColor = false;
-            this.btn_Explore.Click += new System.EventHandler(this.btn_Explore_Click);
-            // 
-            // btn_Add
-            // 
-            this.btn_Add.BackColor = System.Drawing.Color.MidnightBlue;
-            this.btn_Add.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btn_Add.Cursor = System.Windows.Forms.Cursors.No;
-            this.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Add.Font = new System.Drawing.Font("Stencil", 11F);
-            this.btn_Add.ForeColor = System.Drawing.Color.AliceBlue;
-            this.btn_Add.Image = ((System.Drawing.Image)(resources.GetObject("btn_Add.Image")));
-            this.btn_Add.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btn_Add.Location = new System.Drawing.Point(254, 39);
-            this.btn_Add.Name = "btn_Add";
-            this.btn_Add.Size = new System.Drawing.Size(214, 53);
-            this.btn_Add.TabIndex = 2;
-            this.btn_Add.Text = "Add";
-            this.btn_Add.UseVisualStyleBackColor = false;
-            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
-            // 
             // panel2
             // 
             this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
             this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panel2.Controls.Add(this.btn_Explore);
+            this.panel2.Controls.Add(this.btn_Delete);
+            this.panel2.Controls.Add(this.btn_Update);
             this.panel2.Controls.Add(this.pictureBox4);
             this.panel2.Controls.Add(this.pictureBox3);
             this.panel2.Controls.Add(this.pictureBox2);
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Controls.Add(this.LogOut);
-            this.panel2.Controls.Add(this.ViewCart);
-            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.Add);
+            this.panel2.Controls.Add(this.Explore);
             this.panel2.Location = new System.Drawing.Point(0, -1);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(800, 600);
             this.panel2.TabIndex = 33;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Stencil", 14F);
-            this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.label3.Location = new System.Drawing.Point(86, 236);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 22);
-            this.label3.TabIndex = 34;
-            this.label3.Text = "Explore";
-            // 
-            // ViewCart
-            // 
-            this.ViewCart.AutoSize = true;
-            this.ViewCart.BackColor = System.Drawing.Color.Transparent;
-            this.ViewCart.Font = new System.Drawing.Font("Stencil", 14F);
-            this.ViewCart.ForeColor = System.Drawing.Color.AliceBlue;
-            this.ViewCart.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.ViewCart.Location = new System.Drawing.Point(86, 311);
-            this.ViewCart.Name = "ViewCart";
-            this.ViewCart.Size = new System.Drawing.Size(46, 22);
-            this.ViewCart.TabIndex = 36;
-            this.ViewCart.Text = "add";
-            // 
-            // LogOut
-            // 
-            this.LogOut.AutoSize = true;
-            this.LogOut.BackColor = System.Drawing.Color.Transparent;
-            this.LogOut.Font = new System.Drawing.Font("Stencil", 14F);
-            this.LogOut.ForeColor = System.Drawing.Color.AliceBlue;
-            this.LogOut.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.LogOut.Location = new System.Drawing.Point(86, 394);
-            this.LogOut.Name = "LogOut";
-            this.LogOut.Size = new System.Drawing.Size(80, 22);
-            this.LogOut.TabIndex = 37;
-            this.LogOut.Text = "logout";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(318, 287);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(164, 60);
-            this.pictureBox1.TabIndex = 38;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(326, 295);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(164, 60);
-            this.pictureBox2.TabIndex = 39;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(334, 303);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(164, 60);
-            this.pictureBox3.TabIndex = 40;
-            this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
@@ -230,24 +106,93 @@
             this.pictureBox4.TabIndex = 41;
             this.pictureBox4.TabStop = false;
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(334, 303);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(164, 60);
+            this.pictureBox3.TabIndex = 40;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(326, 295);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 60);
+            this.pictureBox2.TabIndex = 39;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(318, 287);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 60);
+            this.pictureBox1.TabIndex = 38;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LogOut
+            // 
+            this.LogOut.AutoSize = true;
+            this.LogOut.BackColor = System.Drawing.Color.Transparent;
+            this.LogOut.Font = new System.Drawing.Font("Stencil", 14F);
+            this.LogOut.ForeColor = System.Drawing.Color.AliceBlue;
+            this.LogOut.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.LogOut.Location = new System.Drawing.Point(73, 359);
+            this.LogOut.Name = "LogOut";
+            this.LogOut.Size = new System.Drawing.Size(80, 22);
+            this.LogOut.TabIndex = 37;
+            this.LogOut.Text = "logout";
+            this.LogOut.Click += new System.EventHandler(this.LogOut_Click);
+            // 
+            // Add
+            // 
+            this.Add.AutoSize = true;
+            this.Add.BackColor = System.Drawing.Color.Transparent;
+            this.Add.Font = new System.Drawing.Font("Stencil", 14F);
+            this.Add.ForeColor = System.Drawing.Color.AliceBlue;
+            this.Add.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Add.Location = new System.Drawing.Point(86, 313);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(46, 22);
+            this.Add.TabIndex = 36;
+            this.Add.Text = "add";
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Explore
+            // 
+            this.Explore.AutoSize = true;
+            this.Explore.BackColor = System.Drawing.Color.Transparent;
+            this.Explore.Font = new System.Drawing.Font("Stencil", 14F);
+            this.Explore.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Explore.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Explore.Location = new System.Drawing.Point(86, 236);
+            this.Explore.Name = "Explore";
+            this.Explore.Size = new System.Drawing.Size(91, 22);
+            this.Explore.TabIndex = 34;
+            this.Explore.Text = "Explore";
+            this.Explore.Click += new System.EventHandler(this.Explore_Click);
+            // 
             // Admin_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 598);
-            this.Controls.Add(this.btn_Add);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel2);
             this.Name = "Admin_Main";
             this.Text = "Admin_Main";
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -256,12 +201,9 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button btn_Update;
         private System.Windows.Forms.Button btn_Delete;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button btn_Explore;
-        private System.Windows.Forms.Button btn_Add;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label ViewCart;
+        private System.Windows.Forms.Label Explore;
+        private System.Windows.Forms.Label Add;
         private System.Windows.Forms.Label LogOut;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
--- BOOKSTORE/BookCard.cs
+++ BOOKSTORE/BookCard.cs
@@ -6,17 +6,11 @@ namespace BOOKSTORE
 {
     public partial class BookCard : UserControl
     {
-        // Public events for buttons
-        public event EventHandler Button1Click;
-        public event EventHandler Button2Click;
+      
 
         public BookCard()
         {
-            InitializeComponent();
-
-            // Wire up button events
-            btn_Edit.Click += (s, e) => Button1Click?.Invoke(this, e);
-            btn_Delete.Click += (s, e) => Button2Click?.Invoke(this, e);
+            InitializeComponent();         
         }
 
         public void SetBookData(
@@ -26,7 +20,8 @@ namespace BOOKSTORE
             string category,
             string isbn,
             decimal price,
-            int stock)
+            int stock,
+            string description)
         {
             pictureBoxCover.Image = cover;
             lblTitle.Text = title;
@@ -35,6 +30,7 @@ namespace BOOKSTORE
             lblISBN.Text = $"ISBN-10: {isbn}";
             lblPrice.Text = $"Php. {price}";
             lblStock.Text = $"({stock}) on stock";
+            lblDescription.Text = description;
         }
     }
 }
